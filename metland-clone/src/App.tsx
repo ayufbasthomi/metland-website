90df2e8c18a112adff6679cc01bf7e4835ead469
ayufbasthomi <ayufbasthomi@gmail.com>
2025-05-23 11:19:19 +0700

    update seo

--- metland-clone/src/App.tsx
+++ metland-clone/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Tentang from './Tentang';
@@ -8,12 +10,18 @@ import ContactForm from './ContactForm';
 import Footer from './Footer';
 import UnitTypes from './components/UnitTypes';
 import Facilities from './components/Facilities';
-import WhatsAppFloatingButton from './components/WhatsAppFloatingButton'; // ✅ Add this
+import WhatsAppFloatingButton from './components/WhatsAppFloatingButton';
 import UpdateSection from './components/UpdateSection';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Metland Cikarang - Rumah Strategis & Modern</title>
+        <meta name="description" content="Metland Cikarang menawarkan rumah minimalis dengan harga terjangkau di lokasi strategis." />
+        <meta name="keywords" content="metland cikarang, rumah murah cikarang, cluster cikarang" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
       <Navbar />
       <div id="hero"><Hero /></div>
       <div id="update"><UpdateSection /></div>
@@ -24,7 +32,7 @@ function App() {
       <div id="lokasi"><LocationMap /></div>
       <div id="kontak"><ContactForm /></div>
       <Footer />
-      <WhatsAppFloatingButton /> {/* ✅ Add this here */}
+      <WhatsAppFloatingButton />
     </div>
   );
 }
--- metland-clone/src/main.tsx
+++ metland-clone/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Make sure there is a <div id='root'></div> in your index.html");
+}
